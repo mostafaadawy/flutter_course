f2479d0f4ed9fff91ccff6620696c588ef6956d1
Mostafa Sayed Adawy <mostafa_adawy@ymail.com>
2023-07-04 13:52:47 +0300

    multiple constractor

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -5,8 +5,15 @@ const beginAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 class GradientContainer extends StatelessWidget {
   const GradientContainer(this.color1, this.color2, {super.key});
+
+  // multiple constractur component/class
+  const GradientContainer.purple({super.key}):
+  color1= Colors.deepPurple,
+  color2= Colors.indigo;
+  
   final Color color1;
   final Color color2;
+  
   @override
   Widget build(context){
       return Container(
