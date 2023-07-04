8d64db56c2a01dfc2f567bc678581b73efeda984
Mostafa Sayed Adawy <mostafa_adawy@ymail.com>
2023-07-04 14:43:51 +0300

    still not working but why?

---
+++ lib/dice_roller.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class DiceRoller extends StatefulWidget{
+  const DiceRoller({super.key});
+  @override // to write over main build or createstate or other existing function on the partent
+  State<DiceRoller> createState() {   // is the alternative to build used in stateless
+    return _DiceRollerState();
+  }
+}
+
+// when using statefull we deal with 2 classes stateful and its state
+class _DiceRollerState extends State<DiceRoller>{
+  var diceImagePath='assets/images/dice-2.png';
+  void rollDice() {
+    diceImagePath='assets/images/dice-4.png';
+    // print('changing image');
+  }
+  @override // to write over main build or createstate or other existing function on
+  Widget build(context){
+    return Column(
+          mainAxisSize: MainAxisSize
+              .min, // allow not to take all vertical but just fit to childern hightes
+          children: [
+            Image.asset(
+              diceImagePath,
+              width: 200,
+            ),
+            const SizedBox(height: 20,), // second method for padding
+            TextButton(
+                // onPressed: (){}), //anonymous function executable without name
+                onPressed: rollDice,
+                style: TextButton.styleFrom(
+                  // padding: const EdgeInsets.only(top: 20),
+                  foregroundColor: Colors.white,
+                  textStyle: const TextStyle(
+                    fontSize: 28,
+                  ),
+                ),
+                child: const Text('Roll Dice'))
+          ],
+        );
+  }
+}
--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,7 +1,7 @@
 // nameing convension requires to replace capital by small chars and seperate by _
 import 'package:flutter/material.dart';
-import 'package:flutter_course/styled_text.dart';
-
+// import 'package:flutter_course/styled_text.dart';
+import 'package:flutter_course/dice_roller.dart';
 const beginAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
@@ -9,18 +9,12 @@ class GradientContainer extends StatelessWidget {
   const GradientContainer(this.color1, this.color2, {super.key}); // reason it doesnt work cause the part that needs changes must be in statfull not in stateless class so we will divide this widget stateless and statfull into two parts
 
   // multiple constractur component/class
-   GradientContainer.purple({super.key})
+  const GradientContainer.purple({super.key})
       : color1 = Colors.deepPurple,
         color2 = Colors.indigo;
 
   final Color color1;
   final Color color2;
-
-  var diceImagePath='assets/images/dice-2.png';
-  void rollDice() {
-    diceImagePath='assets/images/dice-4.png';
-    print('changing image');
-  }
 
   @override
   Widget build(context) {
@@ -36,28 +30,7 @@ class GradientContainer extends StatelessWidget {
           ),
         ),
         child: Center(
-            child: Column(
-          mainAxisSize: MainAxisSize
-              .min, // allow not to take all vertical but just fit to childern hightes
-          children: [
-            Image.asset(
-              diceImagePath,
-              width: 200,
-            ),
-            const SizedBox(height: 20,), // second method for padding
-            TextButton(
-                // onPressed: (){}), //anonymous function executable without name
-                onPressed: rollDice,
-                style: TextButton.styleFrom(
-                  // padding: const EdgeInsets.only(top: 20),
-                  foregroundColor: Colors.white,
-                  textStyle: const TextStyle(
-                    fontSize: 28,
-                  ),
-                ),
-                child: const Text('Roll Dice'))
-          ],
-        )
+            child: DiceRoller(),
             // child: StyledText('Hi Darsh'),
             ));
   }
