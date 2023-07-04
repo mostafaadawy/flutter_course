16eab441adb588faad8954dea9b68defb5f8fdde
Mostafa Sayed Adawy <mostafa_adawy@ymail.com>
2023-07-04 10:01:27 +0300

    changeing bg with container linear gradient

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,22 @@ import 'package:flutter/material.dart';
 // ctrl+shif_r refactor to wrap widget
 // alt_shift+f formate the code
 void main() {
-  runApp(const MaterialApp(
+  runApp(MaterialApp(
     home: Scaffold(
-      backgroundColor: Color.fromARGB(255, 149, 89, 217),
-      body: Center(child: Text('Darash')),
+      body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 26, 2, 80),
+                Color.fromARGB(255, 45, 7, 96),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const Center(
+            child: Text('Darash'),
+          )),
     ),
   ));
 }
