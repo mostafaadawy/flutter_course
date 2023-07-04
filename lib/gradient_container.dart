e66a50377b83e865a08c31f71235db9a76a9cb46
Mostafa Sayed Adawy <mostafa_adawy@ymail.com>
2023-07-04 14:06:05 +0300

    column mainAxisSize to fit the hight to childern

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,45 +1,53 @@
 // nameing convension requires to replace capital by small chars and seperate by _
 import 'package:flutter/material.dart';
 import 'package:flutter_course/styled_text.dart';
+
 const beginAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
+
 class GradientContainer extends StatelessWidget {
   const GradientContainer(this.color1, this.color2, {super.key});
 
   // multiple constractur component/class
-  const GradientContainer.purple({super.key}):
-  color1= Colors.deepPurple,
-  color2= Colors.indigo;
-  
+  const GradientContainer.purple({super.key})
+      : color1 = Colors.deepPurple,
+        color2 = Colors.indigo;
+
   final Color color1;
   final Color color2;
-  
-  void rollDice(){
+
+  void rollDice() {
     //.....
   }
 
   @override
-  Widget build(context){
-      return Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                color1, color2,
-              ],
-              begin: beginAlignment,
-              end: endAlignment,
-            ),
+  Widget build(context) {
+    return Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              color1,
+              color2,
+            ],
+            begin: beginAlignment,
+            end: endAlignment,
           ),
-          child: Center(
-            child:Column(children: [
-                Image.asset('assets/images/dice-1.png',width: 200,),
-                TextButton(
-                  // onPressed: (){}), //anonymous function executable without name 
-                  onPressed: rollDice,
-                  child: const Text('Roll Dice'))
-            ],)
+        ),
+        child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min, // allow not to take all vertical but just fit to childern hightes
+          children: [
+            Image.asset(
+              'assets/images/dice-1.png',
+              width: 200,
+            ),
+            TextButton(
+                // onPressed: (){}), //anonymous function executable without name
+                onPressed: rollDice,
+                child: const Text('Roll Dice'))
+          ],
+        )
             // child: StyledText('Hi Darsh'),
-          )
-          );
+            ));
   }
 }
